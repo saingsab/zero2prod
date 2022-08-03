8e49c7a39af519c125937d1264958c4bd46b76c6
Saing Sab (Ayoung) <saing.sab@gmail.com>
2022-08-03 14:34:33 +0700

    add Test Unit

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,7 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
-
-// HttpRequest
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-// async fn greet(req: HttpRequest) -> impl Responder {
-//     let name = req.match_info().get("name").unwrap_or("World");
-//     format!("Hello {}!", &name)
-// }
+use zero2prod::run;
 
 #[tokio::main]
+
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            // .route("/", web::get().to(greet))
-            // .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run().await
 }
