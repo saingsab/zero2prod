f46856cdc6454538e5e5f8eae5cd23020c22b6d1
Saing Sab (Ayoung) <saing.sab@gmail.com>
2022-08-03 11:56:16 +0700

    adding lib

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use actix_web::{web, App, HttpServer, HttpRequest, Responder};
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+// HttpRequest
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
 }
+
+// async fn greet(req: HttpRequest) -> impl Responder {
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello {}!", &name)
+// }
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            // .route("/", web::get().to(greet))
+            // .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
