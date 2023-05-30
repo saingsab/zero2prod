4e81feb027aad59c9212a45bd540639a1ee59b90
Saing <saing.sab@gmail.com>
2023-05-30 15:12:09 +0700

    adding db connection

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::net::TcpListener;
 use zero2prod::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let address = TcpListener::bind("127.0.0.1:8088")?;
-    run(address)?.await
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
