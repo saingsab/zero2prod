fc8bedc0cd2790833eb18cb13a9dc4798926bc92
Saing <saing.sab@gmail.com>
2023-05-30 16:46:51 +0700

    fix clone

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::net::TcpListener;
-use zero2prod::run;
+// use zero2prod::run;
 use zero2prod::configuration::get_configuration;
+use sqlx::{Connection, PgConnection};
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
---
+++ src/startup.rs
@@ -0,0 +1,22 @@
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
+use std::net::TcpListener;
+
+pub fn run(
+    listener: TcpListener,
+    // New parameter!
+    connection: PgConnection
+) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
