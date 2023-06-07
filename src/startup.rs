db3fdcc091ce298f8cf993d870c5aaba91dd9bec
Saing <saing.sab@gmail.com>
2023-06-07 15:08:55 +0700

    rust analyzer

--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,9 @@ use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use actix_web::middleware::Logger;
+use actix_web::Server;
+use actix_web::Data;
+use tracing_actix_web::TracingLogger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -12,7 +15,7 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
